12f88f8561c32c252608d3dc0d65637e6099415b
barabash <barabash@BARABASH>
2017-10-09 12:37:23 +0700

    Add project files.

---
+++ LQA_New/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LQA_New.Startup))]
+namespace LQA_New
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
